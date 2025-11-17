9c2b737717d4bf1b76f0f2f2aba25ebbf35dc7f0
haopingzz <2429302207@qq.com>
2025-11-17 21:10:34 +0800

    Date_Calc完成

--- Date_Calc/Date_Calc.h
+++ Date_Calc/Date_Calc.h
@@ -5,28 +5,43 @@ using namespace std;
 
 class Date
 {
+	friend ostream& operator<<(ostream& out, const Date& d);
+	friend istream& operator>>(istream& in, Date& d);
 public:
 	Date(int year = 1900, int month = 1, int day = 1);
 	void print();
 	int GetMonthDay(int year, int month)
 	{
+		assert(month > 0 && month < 13);
 		static int MonthDayArray[13] = { -1,31,28,31,30,31,30,31,31,30,31,30,31 };
-		if (month == 2 && ((month % 4 == 0 && month % 100 != 0) || month % 400 == 0))
+		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
 			return 29;
 		return MonthDayArray[month];
 	}
-	bool operator<(const Date& d);
-	bool operator<=(const Date& d);
-	bool operator>(const Date& d);
-	bool operator>=(const Date& d);
-	bool operator==(const Date& d);
-	bool operator!=(const Date& d);
+	bool CheckDate();
+	bool operator<(const Date& d)const;
+	bool operator<=(const Date& d)const;
+	bool operator>(const Date& d)const;
+	bool operator>=(const Date& d)const;
+	bool operator==(const Date& d)const;
+	bool operator!=(const Date& d)const;
 	
 	Date operator+(int day);
-	Date operator+=(int day);
+	Date& operator+=(int day);
 	Date operator-(int day);
+	Date& operator-=(int day);
+	Date operator++(int);
+	Date& operator++();
+	Date operator--(int);
+	Date& operator--();
+
+	int operator-(const Date& d) const;
+
 private:
 	int _year;
 	int _month;
 	int _day;
 };
+
+ostream& operator<<(ostream& out, const Date& d);
+istream& operator>>(istream& in, const Date& d);
--- Date_Calc/test.cpp
+++ Date_Calc/test.cpp
@@ -1,11 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include"Date_Calc.h"
 
-int main()
+void test01()
 {
-	Date d1(2025, 11, 16);
-	Date d2 = d1 + 100;
+	Date d1(2025, 5, 7);
+	Date d2 = d1 + 10000;
 	d1.print();
 	d2.print();
+
+	Date d3(2025, 11, 17);
+	Date d4 = d3 - 10000;
+	d3.print();
+	d4.print();
+	
+	d3 += -10000;
+	d3.print();
+}
+
+void test02()
+{
+	Date d1(2025, 11, 17);
+	Date d2(2025, 5, 7);
+	int n = d1 - d2;
+	cout << n << endl;
+}
+
+int main()
+{
+	test01();
+	test02();
 	return 0;
 }
