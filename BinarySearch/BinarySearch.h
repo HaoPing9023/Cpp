f30ef639c3adc7a8e330448d8d14f2adcb5f937a
haopingzz <2429302207@qq.com>
2025-11-30 03:07:30 +0800

    完善key_value内容

--- BinarySearch/BinarySearch.h
+++ BinarySearch/BinarySearch.h
@@ -205,3 +205,210 @@ namespace key
 		Node* _root = nullptr;
 	};
 }
+
+
+namespace key_value
+{
+	template<class K, class V>
+	struct BSTNode
+	{
+		K _key;
+		V _value;
+		BSTNode<K, V>* _left;
+		BSTNode<K, V>* _right;
+
+		BSTNode(const K& key, const V& value)
+			:_key(key)
+			,_value(value)
+			, _left(nullptr)
+			, _right(nullptr)
+		{}
+	};
+
+	template<class K, class V>
+	class BSTree
+	{
+		//typedef BSTNode<K> Node;
+		using Node = BSTNode<K, V>;
+	public:
+		BSTree() = default;
+
+		bool Insert(const K& key, const V& value)
+		{
+			if (_root == nullptr)
+			{
+				_root = new Node(key, value);
+				return true;
+			}
+
+			Node* parent = nullptr;
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			cur = new Node(key, value);
+			if (parent->_key < cur->_key)
+			{
+				parent->_right = cur;
+			}
+			else
+			{
+				parent->_left = cur;
+			}
+
+			return true;
+		}
+
+		Node* Find(const K& key)
+		{
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					cur = cur->_left;
+				}
+				else
+				{
+					return cur;
+				}
+			}
+			return nullptr;
+		}
+
+		bool Erase(const K& key)
+		{
+			Node* parent = nullptr;
+			Node* cur = _root;
+
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				//找到后
+				else
+				{
+					//若左子树为空
+					if (cur->_left == nullptr)
+					{
+						if (cur == _root)
+						{
+							_root = cur->_right;
+						}
+						else
+						{
+							if (parent->_left == cur)
+							{
+								parent->_left = cur->_right;
+							}
+							else
+							{
+								parent->_right = cur->_right;
+							}
+						}
+
+						delete cur;
+
+					}
+
+					//若右子树为空
+					else if (cur->_right == nullptr)
+					{
+						if (cur == _root)
+						{
+							_root = cur->_left;
+						}
+						else
+						{
+							if (parent->_left == cur)
+							{
+								parent->_left = cur->_left;
+							}
+							else
+							{
+								parent->_right = cur->_left;
+							}
+						}
+
+						delete cur;
+
+					}
+
+					else
+					{
+						//左右都不为空
+						//删右子树的最左点
+						Node* replaceParent = cur;
+						Node* replace = cur->_right;
+						while (replace->_left)
+						{
+							replaceParent = replace;
+							replace = replace->_left;
+						}
+
+						cur->_key = replace->_key;
+
+						if (replaceParent->_left == replace)
+							replaceParent->_left = replace->_right;
+						else
+							replaceParent->_right = replace->_right;
+
+						delete replace;
+
+					}
+
+					return true;
+				}
+			}
+			return false;
+		}
+
+		void InOrder()
+		{
+			_InOrder(_root);
+			cout << endl;
+		}
+
+	private:
+
+		void _InOrder(Node* root)
+		{
+			if (root == nullptr)
+			{
+				return;
+			}
+			_InOrder(root->_left);
+			cout << root->_key << ":" << root->_value << " ";
+			_InOrder(root->_right);
+		}
+
+		Node* _root = nullptr;
+	};
+}
