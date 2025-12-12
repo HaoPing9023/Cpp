45400ba13f8a0fea2b9f3392a2c424eda2a58765
haopingzz <2429302207@qq.com>
2025-12-12 22:35:45 +0800

    哈希模拟测试

--- Hash/test.cpp
+++ Hash/test.cpp
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include"HashTable.h"
+#include<set>
+#include<unordered_set>
 
 void test01()
 {
@@ -26,8 +28,87 @@ void test01()
 	}
 }
 
+void test02()
+{
+	const size_t N = 10000;
+	unordered_set<int> us;
+	set<int> s;
+	vector<int> v;
+	v.reserve(N);
+	srand(time(0));
+	for (size_t i = 0; i < N; ++i)
+	{
+		//v.push_back(rand()); // N比较大时，重复值比较多
+		//v.push_back(rand() + i); // 重复值相对少
+		v.push_back(i); // 没有重复，有序
+	}
+
+	size_t begin1 = clock();
+	for (auto e : v)
+	{
+		s.insert(e);
+	}
+	size_t end1 = clock();
+	cout << "set insert:" << end1 - begin1 << endl;
+
+	size_t begin2 = clock();
+	us.reserve(N);
+	for (auto e : v)
+	{
+		us.insert(e);
+	}
+	size_t end2 = clock();
+	cout << "unordered_set insert:" << end2 - begin2 << endl;
+
+	cout << "插入数据个数：" << s.size() << endl;
+	cout << "插入数据个数：" << us.size() << endl << endl;
+
+	int m1 = 0;
+	size_t begin3 = clock();
+	for (auto e : v)
+	{
+		auto ret = s.find(e);
+		if (ret != s.end())
+		{
+			++m1;
+		}
+	}
+	size_t end3 = clock();
+	cout << "set find:" << end3 - begin3 << "->" << m1 << endl;
+
+	int m2 = 0;
+	size_t begin4 = clock();
+	for (auto e : v)
+	{
+		auto ret = us.find(e);
+		if (ret != us.end())
+		{
+			++m2;
+		}
+	}
+	size_t end4 = clock();
+	cout << "unorered_set find:" << end4 - begin4 << "->" << m2 << endl;
+
+	size_t begin5 = clock();
+	for (auto e : v)
+	{
+		s.erase(e);
+	}
+	size_t end5 = clock();
+	cout << "set erase:" << end5 - begin5 << endl;
+	size_t begin6 = clock();
+
+	for (auto e : v)
+	{
+		us.erase(e);
+	}
+	size_t end6 = clock();
+	cout << "unordered_set erase:" << end6 - begin6 << endl << endl;
+}
+
 int main()
 {
-	test01();
+	//test01();
+	test02();
 	return 0;
 }
