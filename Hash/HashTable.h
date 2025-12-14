a9994007cb76e451e4c0f24ef7f356aca1960dad
Hao_Ping <2429302207@qq.com>
2025-12-14 21:38:36 +0800

    补充哈希桶模拟

--- Hash/HashTable.h
+++ Hash/HashTable.h
@@ -10,6 +10,24 @@ enum State
 	DELETE
 };
 
+inline unsigned long __stl_next_prime(unsigned long n)
+{
+	// Note: assumes long is at least 32 bits.
+	static const int __stl_num_primes = 28;
+	static const unsigned long __stl_prime_list[__stl_num_primes] = {
+		53, 97, 193, 389, 769,
+		1543, 3079, 6151, 12289, 24593,
+		49157, 98317, 196613, 393241, 786433,
+		1572869, 3145739, 6291469, 12582917, 25165843,
+		50331653, 100663319, 201326611, 402653189, 805306457,
+		1610612741, 3221225473, 4294967291
+	};
+	const unsigned long* first = __stl_prime_list;
+	const unsigned long* last = __stl_prime_list + __stl_num_primes;
+	const unsigned long* pos = lower_bound(first, last, n);
+	return pos == last ? *(last - 1) : *pos;
+}
+
 template<class K, class V>
 struct HashData
 {
@@ -17,137 +35,213 @@ struct HashData
 	State _state = EMPTY;
 };
 
-template<class K, class V>
-class HashTable
+template<class K>
+struct HashFunc
 {
-public:
-	HashTable()
-		:_tables(__stl_next_prime(0))
-		, _n(0)
-	{}
-
-	inline unsigned long __stl_next_prime(unsigned long n)
+	size_t operator()(const K& key)
 	{
-		// Note: assumes long is at least 32 bits.
-		static const int __stl_num_primes = 28;
-		static const unsigned long __stl_prime_list[__stl_num_primes] = {
-			53, 97, 193, 389, 769,
-			1543, 3079, 6151, 12289, 24593,
-			49157, 98317, 196613, 393241, 786433,
-			1572869, 3145739, 6291469, 12582917, 25165843,
-			50331653, 100663319, 201326611, 402653189, 805306457,
-			1610612741, 3221225473, 4294967291
-		};
-		const unsigned long* first = __stl_prime_list;
-		const unsigned long* last = __stl_prime_list + __stl_num_primes;
-		const unsigned long* pos = lower_bound(first, last, n);
-		return pos == last ? *(last - 1) : *pos;
+		return (size_t)key;
 	}
+};
 
-	bool Insert(const pair<K, V>& kv)
+template<>
+struct HashFunc<string>
+{
+	size_t operator()(const string s)
 	{
-		if (Find(kv.first))
-			return false;
-		//负载因子
-		// n / 容量 要 >= 0.7
-		if (_n * 10 / _tables.size() >= 7)
+		size_t hash = 0;
+		for (auto& e : s)
 		{
-			//vector<HashTable<K, V>> newtables(_tables.size() * 2);
-			//for (auto& data : _tables)
-			//{
-			//	if (data._state == EXIST)
-			//	{
-			//		size_t Hash0 = kv.first % _tables.size();
-			//		//......	冗余
-			//	}
-			//}
-			//_tables.swap(newtables);
+			hash += e;
+			hash *= 131;
+		}
 
-			HashTable<K, V> newht;
-			newht._tables.resize(_tables.size() * 2);
-			for (auto& data : _tables)
+		return hash;
+	}
+};
+
+namespace open_address
+{
+	template<class K, class V, class Hash = HashFunc<K>>
+	class HashTable
+	{
+	public:
+		HashTable()
+			:_tables(__stl_next_prime(0))
+			, _n(0)
+		{}
+
+		bool Insert(const pair<K, V>& kv)
+		{
+			if (Find(kv.first))
+				return false;
+			//负载因子
+			// n / 容量 要 >= 0.7
+			if (_n * 10 / _tables.size() >= 7)
 			{
-				//旧表的数据映射到新表
-				if (data._state == EXIST)
+				//vector<HashTable<K, V>> newtables(_tables.size() * 2);
+				//for (auto& data : _tables)
+				//{
+				//	if (data._state == EXIST)
+				//	{
+				//		size_t Hash0 = kv.first % _tables.size();
+				//		//......	冗余
+				//	}
+				//}
+				//_tables.swap(newtables);
+
+				HashTable<K, V, Hash> newht;
+				newht._tables.resize(__stl_next_prime(_tables.size() + 1));
+				for (auto& data : _tables)
 				{
-					newht.Insert(data._kv);
+					//旧表的数据映射到新表
+					if (data._state == EXIST)
+					{
+						newht.Insert(data._kv);
+					}
 				}
+
+				_tables.swap(newht._tables);
 			}
-			
-			_tables.swap(newht._tables);
-		}
 
-		size_t Hash0 = kv.first % _tables.size();
-		size_t Hashi = Hash0;
-		size_t i = 1;
+			size_t Hash0 = kv.first % _tables.size();
+			size_t Hashi = Hash0;
+			size_t i = 1;
 
-		while (_tables[Hashi]._state == EXIST)
-		{
-			//线性探测
-			Hashi = (Hash0 + i) % _tables.size();
-			++i;
-		}
-		
-		_tables[Hashi]._kv = kv;
-		_tables[Hashi]._state = EXIST;
-
-		++_n;
-		return true;
-	}
-
-	HashData<K, V>* Find(const K& key)
-	{
-		size_t Hash0 = key % _tables.size();
-		size_t Hashi = Hash0;
-		size_t i = 1;
-		int flag = 1;
-		
-		while (_tables[Hashi]._state != EMPTY)
-		{
-			if (_tables[Hashi]._state == EXIST 
-				&& _tables[Hashi]._kv.first == key)
+			while (_tables[Hashi]._state == EXIST)
 			{
-				return &_tables[Hashi];
+				//线性探测
+				Hashi = (Hash0 + i) % _tables.size();
+				++i;
 			}
 
-			//线性探测
-			Hashi = (Hash0 + i) % _tables.size();
-			++i;
+			_tables[Hashi]._kv = kv;
+			_tables[Hashi]._state = EXIST;
 
-			////二次探测
-			//Hashi = (Hash0 + (i * i * flag)) % _tables.size();
-			//
-			//if (Hashi < _tables.size())
-			//	Hashi += _tables.size();
-
-			//if (flag == 1)
-			//{
-			//	flag = -1;
-			//}
-			//else
-			//{
-			//	flag = 1;
-			//	++i;
-			//}
-		}
-		return nullptr;
-	}
-
-	bool Erase(const K& key)
-	{
-		HashData<K, V>* ret = Find(key);
-		if (ret)
-		{
-			ret->_state = DELETE;
+			++_n;
 			return true;
 		}
-		else
+
+		HashData<K, V>* Find(const K& key)
 		{
-			return false;
+			size_t Hash0 = key % _tables.size();
+			size_t Hashi = Hash0;
+			size_t i = 1;
+			int flag = 1;
+
+			while (_tables[Hashi]._state != EMPTY)
+			{
+				if (_tables[Hashi]._state == EXIST
+					&& _tables[Hashi]._kv.first == key)
+				{
+					return &_tables[Hashi];
+				}
+
+				//线性探测
+				Hashi = (Hash0 + i) % _tables.size();
+				++i;
+
+				////二次探测
+				//Hashi = (Hash0 + (i * i * flag)) % _tables.size();
+				//
+				//if (Hashi < _tables.size())
+				//	Hashi += _tables.size();
+
+				//if (flag == 1)
+				//{
+				//	flag = -1;
+				//}
+				//else
+				//{
+				//	flag = 1;
+				//	++i;
+				//}
+			}
+			return nullptr;
 		}
 
-	}
-private:
-	vector<HashData<K, V>> _tables;
-	size_t _n;
-};
+		bool Erase(const K& key)
+		{
+			HashData<K, V>* ret = Find(key);
+			if (ret)
+			{
+				ret->_state = DELETE;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+	private:
+		vector<HashData<K, V>> _tables;
+		size_t _n;
+	};
+}
+
+namespace hash_bucket
+{
+	template<class K, class V>
+	struct HashNode
+	{
+		pair<K, V> _kv;
+		HashNode<K, V>* _next;
+
+		HashNode(const pair<K, V> kv)
+			:_kv(kv)
+			, _next(nullptr)
+		{}
+	};
+
+	template<class K, class V, class Hash = HashFunc<K>>
+	class HashTable
+	{
+		typedef HashNode<K, V> Node;
+	public:
+		HashTable()
+			:_tables(11)
+			, _n(0)
+		{}
+
+		bool Insert(const pair<K, V>& kv)
+		{
+			if (_n == _tables.size())
+			{
+				vector<Node*> newTables(_tables.size() * 2);
+				for (size_t i = 0; i < _tables.size(); i++)
+				{
+					Node* cur = _tables[i];
+					while (cur)
+					{
+						Node* next = cur->_next;
+						//头插到新表
+						size_t hashi = cur->_kv.first % newTables.size();
+						cur->_next = newTables[hashi];
+						newTables[hashi] = cur;
+
+						cur = next;
+					}
+					_tables[i] = nullptr;
+				}
+
+				_tables.swap(newTables);
+			}
+
+			size_t hashi = kv.first % _tables.size();
+			
+			//头插
+			Node* newnode = new Node(kv);
+			newnode->_next = _tables[hashi];
+			_tables[hashi] = newnode;
+			++_n;
+			
+			return true;
+		}
+
+	private:
+		vector<Node*> _tables;	// 指针数组
+		size_t _n = 0;	// 表中存储数据个数
+	};
+}
+
--- Hash/test.cpp
+++ Hash/test.cpp
@@ -1,36 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include"HashTable.h"
+
+#include<iostream>
 #include<set>
 #include<unordered_set>
 
-void test01()
+using namespace std;
+
+void test_set1()
 {
-	int a[] = { 19,30,5,36,13,20,21,12 };
-	HashTable<int, int> ht;
-	for (auto e : a)
+	unordered_set<int> s = { 3,1,6,7,8,2,1,1,5,6,7,6 };
+	unordered_set<int>::iterator it = s.begin();
+	while (it != s.end())
 	{
-		ht.Insert({ e,e });
+		cout << *it << " ";
+		++it;
 	}
-
-	ht.Erase(30);
-	if (ht.Find(20))
-	{
-		cout << "找到了" << endl;
-	}
-
-	if (ht.Find(30))
-	{
-		cout << "找到了" << endl;
-	}
-	else
-	{
-		cout << "没有找到" << endl;
-	}
+	cout << endl;
 }
 
-void test02()
+void test_set2()
 {
-	const size_t N = 10000;
+	const size_t N = 10000000;
 	unordered_set<int> us;
 	set<int> s;
 	vector<int> v;
@@ -38,7 +28,7 @@ void test02()
 	srand(time(0));
 	for (size_t i = 0; i < N; ++i)
 	{
-		//v.push_back(rand()); // N比较大时 重复值比较多
+		//v.push_back(rand()); // N比较大时，重复值比较多
 		//v.push_back(rand() + i); // 重复值相对少
 		v.push_back(i); // 没有重复，有序
 	}
@@ -106,9 +96,132 @@ void test02()
 	cout << "unordered_set erase:" << end6 - begin6 << endl << endl;
 }
 
+#include"HashTable.h"
+
+//int main()
+//{
+//	//int a[] = { 19,30,52,63,11,22 };
+//	int a[] = { 19,30,5,36,13,20,21,12 };
+//	HashTable<int, int> ht;
+//	for (auto e : a)
+//	{
+//		ht.Insert({ e, e });
+//	}
+//
+//	//ht.Insert({ 15, 15 });
+//
+//	ht.Erase(30);
+//	if (ht.Find(20))
+//	{
+//		cout << "找到了" << endl;
+//	}
+//
+//	if (ht.Find(30))
+//	{
+//		cout << "找到了" << endl;
+//	}
+//	else
+//	{
+//		cout << "没有找到" << endl;
+//	}
+//
+//	return 0;
+//}
+
+//struct StringHashFunc
+//{
+//	size_t operator()(const string& s)
+//	{
+//		size_t hash = 0;
+//		for (auto ch : s)
+//		{
+//			hash += ch;
+//		}
+//
+//		return hash;
+//	}
+//};
+
+struct Date
+{
+	int _year;
+	int _month;
+	int _day;
+
+	Date(int year = 1, int month = 1, int day = 1)
+		:_year(year)
+		, _month(month)
+		, _day(day)
+	{}
+
+	bool operator==(const Date& d)
+	{
+		return _year == d._year
+			&& _month == d._month
+			&& _day == d._day;
+	}
+};
+
+struct DateHashFunc
+{
+	size_t operator()(const Date& d)
+	{
+		size_t hash = 0;
+		hash += d._year;
+		hash *= 131;
+
+		hash += d._month;
+		hash *= 131;
+
+		hash += d._day;
+		hash *= 131;
+
+		return hash;
+	}
+};
+
+//int main()
+//{
+//	//int a[] = { 19,30,52,63,11,22 };
+//	
+//	const char* a1[] = { "abcd", "sort", "insert" };
+//	HashTable<string, string> ht1;
+//	for (auto& e : a1)
+//	{
+//		ht1.Insert({ e, e });
+//	}
+//
+//	cout << HashFunc<string>()("abcd") << endl;
+//	cout << HashFunc<string>()("bcad") << endl;
+//	cout << HashFunc<string>()("aadd") << endl;
+//
+//	int a2[] = { -19,-30,5,36,13,20,21,12 };
+//	HashTable<int, int> ht2;
+//	for (auto e : a2)
+//	{
+//		ht2.Insert({ e, e });
+//	}
+//
+//	// 哈希冲突
+//	HashTable<Date, int, DateHashFunc> ht;
+//	ht.Insert({ { 2024, 10, 12 }, 1});
+//	ht.Insert({ { 2024, 12, 10 }, 1 });
+//
+//	return 0;
+//}
+
 int main()
 {
-	//test01();
-	test02();
+	int a2[] = { 19,30,5,36,13,20,21,12,24,96 };
+	hash_bucket::HashTable<int, int> ht2;
+	for (auto e : a2)
+	{
+		ht2.Insert({ e, e });
+	}
+
+	ht2.Insert({ 100, 100 });
+	ht2.Insert({ 101, 101 });
+
+
 	return 0;
 }
