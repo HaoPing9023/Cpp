b9fc74692fe3d71097fffb16a7043e0f26819736
Hao_Ping <2429302207@qq.com>
2026-01-09 23:06:54 +0800

    两数之和

---
+++ Algorithm_150/两数之和.cpp
@@ -0,0 +1,21 @@
+//1.两数之和
+//给定一个整数数组 nums 和一个整数目标值 target
+//请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+//你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+//你可以按任意顺序返回答案。
+
+#include"Algorithm_150.h"
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = res.find(target - nums[i]);
+            if (it != res.end()) {
+                return { it->second, i };
+            }
+            res[nums[i]] = i;
+        }
+        return {};
+    }
+};
