5aa6ea476e7d01e2991e4d1231c759e84621b065
Hao_Ping <2429302207@qq.com>
2026-01-16 14:29:09 +0800

    155.最小栈

---
+++ Algorithm_150/最小栈.cpp
@@ -0,0 +1,52 @@
+//155.最小栈
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+//实现 MinStack 类 :
+//	·MinStack() 初始化堆栈对象。
+//	·void push(int val) 将元素val推入堆栈。
+//	·void pop() 删除堆栈顶部的元素。
+//	·int top() 获取堆栈顶部的元素。
+//	·int getMin() 获取堆栈中的最小元素。
+
+#include"Algorithm_150.h"
+
+class MinStack {
+public:
+    MinStack() {
+    }
+
+    void push(int val) {
+        _st.push(val);
+        if (_minst.empty() || val <= _minst.top())
+        {
+            _minst.push(val);
+        }
+    }
+
+    void pop() {
+        if (_st.top() == _minst.top())
+        {
+            _minst.pop();
+        }
+        _st.pop();
+    }
+
+    int top() {
+        return _st.top();
+    }
+
+    int getMin() {
+        return _minst.top();
+    }
+private:
+    stack<int> _st;
+    stack<int> _minst;
+};
+
+
+//Your MinStack object will be instantiated and called as such:
+//MinStack* obj = new MinStack();
+//obj->push(val);
+//obj->pop();
+//int param_3 = obj->top();
+//int param_4 = obj->getMin();
+ 
