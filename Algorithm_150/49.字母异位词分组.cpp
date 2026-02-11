32d2dfc5a704ae3bbda65f3cbd51ed18484ea123
Hao_Ping <2429302207@qq.com>
2026-02-11 21:38:32 +0800

    49.字母异位词分组

---
+++ Algorithm_150/49.字母异位词分组.cpp
@@ -0,0 +1,26 @@
+//给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+//示例:
+//输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出 : [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+
+#include"Algorithm_150.h"
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (auto& s : strs) {
+            string key = s;
+            // ranges::sort(key);   // C++20写法
+            sort(key.begin(), key.end());   // 将字符串排序后存入哈希表
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> res;
+        res.reserve(mp.size());     //可选 预留空间，减少vector扩容次数，提高效率
+        for (auto& s : mp) {
+            res.push_back(s.second);
+        }
+        return res;
+    }
+};
