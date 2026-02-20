e2572319f293c57e241b4776fc5c85f2f91a275c
Hao_Ping <2429302207@qq.com>
2026-02-20 20:49:57 +0800

    234.回文链表

---
+++ Algorithm_150/234.回文链表.cpp
@@ -0,0 +1,31 @@
+//234.回文链表
+//给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。
+//如果是，返回 true ；否则，返回 false 。
+
+#include"Algorithm_150.h"
+
+//Definition for singly-linked list.
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> vals;
+        while (head != nullptr) {
+            vals.emplace_back(head->val);
+            head = head->next;
+        }
+        for (int i = 0, j = (int)vals.size() - 1; i < j; ++i, --j) {
+            if (vals[i] != vals[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
